2287f1501db650881a9f5c23e1fb065a51c339b3
PEDRO2221 <anonimousdex01@gmail.com>
2019-11-07 16:53:35 -0300

    v1.3

--- src/main/java/com/clj/monitoria/controller/ControllerIndex.java
+++ src/main/java/com/clj/monitoria/controller/ControllerIndex.java
@@ -1,6 +1,7 @@
 package com.clj.monitoria.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ public class ControllerIndex {
 	
 	
      @GetMapping("/")
+     @PreAuthorize("hasRole('ADMIN')")
      public String Home() {
     	 return "index";
      }    
