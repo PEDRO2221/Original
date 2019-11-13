5f793497f4a2473da7026f91684a3a71d746b567
Marcus <oliveirasilva818@gmail.com>
2019-11-13 09:04:13 -0300

    V1.5

--- src/main/java/com/clj/monitoria/controller/ControllerIndex.java
+++ src/main/java/com/clj/monitoria/controller/ControllerIndex.java
@@ -1,14 +1,17 @@
 package com.clj.monitoria.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.clj.monitoria.model.Comidas;
 import com.clj.monitoria.repository.ComidaBD;
 @Controller
@@ -19,16 +22,27 @@ public class ControllerIndex {
 	
      @GetMapping("/")
      @PreAuthorize("hasRole('ADMIN')")
-     public String Home() {
+     public String Home(Model model) {
+    	 model.addAttribute("comidas", comida.findAll());
     	 return "index";
-     }    
-     @GetMapping("/cadastro")
-     public String cadastro() {
-		return "cadastro";
-    	 
-     }
+     }  
+ 	@GetMapping("/cad")
+ 	public String home() {
+ 		return "cadastro";
+ 	}
+
+ 	@GetMapping("/cadastro")
+ 	public String enviar(@Valid Comidas cmd,BindingResult result
+ 			, RedirectAttributes attributes) {
+ 				if (result.hasErrors()) {
+
+ 		}
+ 		attributes.addFlashAttribute("mensagem", "Receita salva com sucesso.");
+ 		comida.save(cmd);		
+ 		return "redirect:/cad";
+ 	}
      
-    @GetMapping("/deletar/{id}")
+   /* @GetMapping("/deletar/{id}")
     public String delete(@PathVariable("id") Long id) {
     	comida.deleteById(id);
     	return "redirect:/";
@@ -37,7 +51,7 @@ public class ControllerIndex {
     public String editar(@PathVariable("id") Long id,Model model) {
     	model.addAttribute("Comidas" , comida.findById(id));
     	return "editar";
-     }
+     }*/
      
      
      
--- src/main/java/com/clj/monitoria/model/Comidas.java
+++ src/main/java/com/clj/monitoria/model/Comidas.java
@@ -1,19 +1,36 @@
 package com.clj.monitoria.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Comidas {
 	@Id
 	@GeneratedValue
-    public Long id;
-    private String nome;
-  	
+    private Long id;
+	@NotNull(message = "nome é obrigatoria!")
+	@Column
+	private String nome;
+	@NotNull(message = "descrição é obrigatoria!")
+	@Column
+    private String descricao; 
+	@NotNull(message = "tempo é obrigatoria!")
+	@Column
   	private double tempo;
+	@NotNull(message = "porção é obrigatoria!")
+	@Column
+  	private double porcao;
+	
 
-  	
+  	public double getPorcao() {
+  		return porcao;
+  	}
+  	public void setPorcao(double porcao) {
+  		this.porcao = porcao;
+  	}
   	public Long getId() {
 		return id;
 	}
@@ -21,13 +38,20 @@ public class Comidas {
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNome() {
 		return nome;
 	}
 
 	public void setNome(String nome) {
-		this.nome = nome;
+		this.nome = nome	;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 
 	public double getTempo() {
