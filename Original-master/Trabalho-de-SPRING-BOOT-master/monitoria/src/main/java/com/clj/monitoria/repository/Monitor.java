8cc40f2dcebbb3e925c8c098cb027f2786838585
Marcus <oliveirasilva818@gmail.com>
2019-11-22 11:10:30 -0300

    v5.8

--- Original-master/Trabalho-de-SPRING-BOOT-master/monitoria/src/main/java/com/clj/monitoria/repository/Monitor.java
+++
@@ -1,9 +0,0 @@
-package com.clj.monitoria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.clj.monitoria.model.Monitores;
-
-public interface Monitor extends JpaRepository<Monitores, Long> {
-
-}
