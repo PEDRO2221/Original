0b8306e1c97c5157152668a5efa8dafdbad13a3e
bobermanBR <tallesbraga63@gmail.com>
2019-11-27 07:43:19 -0300

    v28

--- src/main/java/com/clj/monitoria/controller/ControllerIndex.java
+++ src/main/java/com/clj/monitoria/controller/ControllerIndex.java
@@ -3,14 +3,12 @@ package com.clj.monitoria.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.clj.monitoria.model.Comidas;
 import com.clj.monitoria.repository.ComidaBD;
@@ -19,16 +17,25 @@ public class ControllerIndex {
 	@Autowired
 	ComidaBD comida;
 
-		@GetMapping("/")
-     @PreAuthorize("hasRole('ADMIN')")
+
+     @GetMapping("/")
      public String Home(Model model) {
+    	 model.addAttribute("comidas", comida.findAll());
+    	 return "home";
+     }
+     
+     
+     @GetMapping("/telInicial")
+     public String user(Model model) {
     	 model.addAttribute("comidas", comida.findAll());
     	 return "index";
      }
- 	@GetMapping("/cadastro")
+     
+ 	@GetMapping("/cad")
  	public String home() {
  		return "cadastro";
  	}
+ 	
 
  	@GetMapping("/cadastro")
  	public String enviar(@Valid Comidas cmd,BindingResult result
@@ -38,6 +45,8 @@ public class ControllerIndex {
  		}
  		attributes.addFlashAttribute("mensagem", "Receita salva com sucesso.");
  		comida.save(cmd);
+ 		
+ 			
  		return "redirect:/";
  	}
 
