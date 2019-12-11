6eb476af147770a5c20d5bb9be6a656bc699d24e
bobermanBR <tallesbraga63@gmail.com>
2019-12-11 07:53:08 -0300

    43

--- src/main/java/com/clj/monitoria/controller/ControllerIndex.java
+++ src/main/java/com/clj/monitoria/controller/ControllerIndex.java
@@ -1,16 +1,17 @@
 package com.clj.monitoria.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.clj.monitoria.model.Comidas;
 import com.clj.monitoria.repository.ComidaBD;
@@ -19,16 +20,39 @@ public class ControllerIndex {
 	@Autowired
 	ComidaBD comida;
 
+<<<<<<< HEAD
      @PreAuthorize("hasRole('ADMIN')")
 		 @GetMapping("/")
+=======
+
+     @GetMapping("/")
+>>>>>>> 1c4962235244ac20af2ec959ff0ea92f28aad8ad
      public String Home(Model model) {
+    	 model.addAttribute("comidas", comida.findAll());
+    	 return "home";
+     }
+     @GetMapping("/listar")
+     public String list(Model model) {
+    	 model.addAttribute("comidas", comida.findAll());
+    	 return "receitas";
+     }
+     
+     
+     @GetMapping("/telInicial")
+     public String user(Model model) {
     	 model.addAttribute("comidas", comida.findAll());
     	 return "index";
      }
- 	@GetMapping("/cadastro")
+     @ModelAttribute("modo")
+     public List<Comidas> comidas() {
+         return comida.findAll();
+     }
+     
+ 	@GetMapping("/cad")
  	public String home() {
  		return "cadastro";
  	}
+ 	
 
  	@GetMapping("/cadastro")
  	public String enviar(@Valid Comidas cmd,BindingResult result
@@ -38,6 +62,8 @@ public class ControllerIndex {
  		}
  		attributes.addFlashAttribute("mensagem", "Receita salva com sucesso.");
  		comida.save(cmd);
+ 		
+ 			
  		return "redirect:/";
  	}
 
@@ -51,7 +77,7 @@ public class ControllerIndex {
 	@GetMapping("/editar/{id}")
 	public String editar(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("comidas", comida.findById(id));
-		return "editar.html";
+		return "editar";
 
 	}
 
