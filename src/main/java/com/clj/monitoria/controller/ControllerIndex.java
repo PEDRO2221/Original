4ed4dacb1ccc55f53b82c5df5ece8b237c32867e
PEDRO2221 <anonimousdex01@gmail.com>
2019-12-11 09:15:21 -0300

    v61

--- src/main/java/com/clj/monitoria/controller/ControllerIndex.java
+++ src/main/java/com/clj/monitoria/controller/ControllerIndex.java
@@ -63,7 +63,7 @@ public class ControllerIndex {
 	}
 
 	@GetMapping("/editar/{id}")
-	public String editar(@PathVariable("id") Long id, Model model, RedirectAttributes attributes) {
+	public String editar(@PathVariable("id") Long id, Model model) {
 		model.addAttribute("comidas", comida.findById(id));
 		return "editar";
 
